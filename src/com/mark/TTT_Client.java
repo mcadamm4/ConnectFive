9248b08718eba328d4ba9ab94a68eeb4b7d0efdc
mcadamm4 <mark.mcadam4@mail.dcu.ie>
2019-01-11 22:32:41 +0000

    Client can connect to the server and receive a response

--- src/com/mark/TTT_Client.java
+++ src/com/mark/TTT_Client.java
@@ -1,8 +1,27 @@
 package com.mark;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class TTT_Client {
 
-    public TTT_Client(String serverAddress) {
+    //    Construct a client and connects to the server
+    private static final String HOST = "localhost";
+    private static final int PORT = 9090;
 
+    public TTT_Client() throws Exception {
+        // Setup connection to server
+        try (Socket socket = new Socket(HOST, PORT)) {
+            System.out.println("Connection status: " + socket.isConnected());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            String response = in.readLine();
+            System.out.println(response);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        TTT_Client client = new TTT_Client();
     }
 }
--- src/com/mark/TTT_Server.java
+++ src/com/mark/TTT_Server.java
@@ -1,8 +1,11 @@
 package com.mark;
 
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TTT_Server {
+    private static final String HOST = "localhost";
     private static final int PORT = 9090;
 
     private String currentPlayer;
@@ -11,8 +14,11 @@ public class TTT_Server {
         try(ServerSocket listener = new ServerSocket(PORT)) {
             System.out.println("Connect Five server is running...");
             while(true) {
-                // Server continues to run until stopped
-                System.out.println("Still running....");
+                try (Socket socket = listener.accept()) {
+                    System.out.println("New Connection");
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println("Connected");
+                }
             }
         }
     }
