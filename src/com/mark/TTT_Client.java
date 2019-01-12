5d39431e2b82924042d6dad0b1e408ec6858ca97
mcadamm4 <mark.mcadam4@mail.dcu.ie>
2019-01-12 01:16:15 +0000

    Players Threads
    PLayers have their own threads and individual data on the server

--- src/com/mark/TTT_Client.java
+++ src/com/mark/TTT_Client.java
@@ -4,24 +4,95 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TTT_Client {
-
-    //    Construct a client and connects to the server
+    private final BufferedReader in;
+    private final PrintWriter out;
     private static final String HOST = "localhost";
     private static final int PORT = 9090;
 
-    public TTT_Client() throws Exception {
+    private static final int WIDTH = 9;
+    private static final int HEIGHT = 6;
+
+    // Define the playing board
+    static String[][] board = new String[HEIGHT][WIDTH];
+
+
+    //    Construct a client and connects to the server
+    public TTT_Client(String playerName, String playerChip) throws Exception {
         // Setup connection to server
         try (Socket socket = new Socket(HOST, PORT)) {
             System.out.println("Connection status: " + socket.isConnected());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            String response = in.readLine();
-            System.out.println(response);
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            while(true) {
+                out.println(playerName);
+                out.println(playerChip);
+
+                System.out.println(in.readLine());
+                Scanner scanner = new Scanner(System.in);
+                String blah = scanner.nextLine();
+            }
+            // Response can be "All players connected" OR "WAITING FRO ANOTHER PLAYER"
         }
     }
+
+    private void setupBoard() {
+        for(int i = 0; i<HEIGHT; i++) {
+            Arrays.fill(board[i], "[_]");
+        }
+    }
+
+    private void displayBoard() {
+        for(String[] arr : board) {
+            for(String str : arr)
+                System.out.print(str + " ");
+            System.out.println();
+        }
+    }
+
+    private void updateBoard(int coords) {
+
+    }
+
+    private void playGame() {
+        String response;
+        try {
+            response = in.readLine();
+
+            // Waiting for another player?
+
+            // All players connected
+            // Your opponent is x
+            // Display Empty grid
+            // Enter [1- 9]
+            // Display new grid
+            // Opponents move, please wait
+            // Display new grid
+//            updateBoard(5);
+            // Please make your move
+
+        } catch(Exception e) {}
+    }
+
     public static void main(String[] args) throws Exception {
-        TTT_Client client = new TTT_Client();
+        Scanner scanner = new Scanner(System.in);
+        String playerChip = "";
+
+        System.out.println("\n## RULES ##");
+        System.out.println("## To win the game you must get FIVE counters in a continuous row. ##\n" +
+                "## Rows can be horizontal, vertical, or diagonal ##\n" + "## ENJOY! ##\n\n");
+        System.out.println("Please Enter your NAME: ");
+        String playerName = scanner.nextLine();
+        System.out.println("You have entered " + playerName + "\n");
+
+        System.out.println("Please choose X's or O's: ");
+        playerChip = scanner.nextLine();
+
+        System.out.println("You have entered " + playerChip + "\n");
+        TTT_Client client = new TTT_Client(playerName, playerChip);
     }
 }
--- src/com/mark/TTT_Server.java
+++ src/com/mark/TTT_Server.java
@@ -1,5 +1,8 @@
 package com.mark;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,18 +10,54 @@ import java.net.Socket;
 public class TTT_Server {
     private static final String HOST = "localhost";
     private static final int PORT = 9090;
+    private static int playerCount = 0;
 
-    private String currentPlayer;
+    // How many players are connected and who's turn is it
+    private String activePlayer;
 
     public static void main(String[] args) throws Exception {
+        String playerName = null;
         try(ServerSocket listener = new ServerSocket(PORT)) {
             System.out.println("Connect Five server is running...");
             while(true) {
-                try (Socket socket = listener.accept()) {
-                    System.out.println("New Connection");
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    out.println("Connected");
-                }
+                ConnectFive newGame = new ConnectFive();
+                // Create two new players for the game and give each a thread
+                ConnectFive.Player playerOne = newGame.new Player(listener.accept());
+                ConnectFive.Player playerTwo = newGame.new Player(listener.accept());
+                playerOne.start();
+                playerTwo.start();
+            }
+        }
+    }
+}
+
+class ConnectFive {
+
+    private static int playerCount = 0;
+
+    class Player extends Thread {
+        private PrintWriter out;
+        private BufferedReader in;
+        private Socket socket;
+
+        private String playerName;
+        private String playerChip;
+
+        public Player(Socket socket) {
+            this.socket = socket;
+            playerCount++;
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                playerName = in.readLine();
+                playerChip = in.readLine();
+
+                System.out.println(playerName + " has successfully connected to the game!");
+                String message = "| Player Name: " + playerName + " | Player Number: " + playerCount + " | " + " Chip: " + playerChip + " | ";
+
+                out.println(message);
+            } catch (IOException e) {
+                System.out.println("OOps");
             }
         }
     }
